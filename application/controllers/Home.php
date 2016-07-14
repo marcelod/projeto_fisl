e7608931346e107ef5341d35c713c9637b11b69f
Marcelo Diniz <marcelo.leo27@gmail.com>
2016-07-14 15:26:59 -0300

    separado as partes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,9 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('include/topo', array('titulo' => 'HOME'));
+		$this->load->view('include/menu', array('menu_active' => 'home'));
 		$this->load->view('home');
+		$this->load->view('include/rodape');
 	}
 }
---
+++ application/views/include/topo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <base href="<?php echo base_url(); ?>">
+
+    <title><?php echo $titulo ?></title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/navbar-fixed-top.css" rel="stylesheet">
+  </head>
+
+  <body>
